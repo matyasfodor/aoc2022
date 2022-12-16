1b1bc6546f5be5c9b6976961b4683eac64304079
Matyas Fodor <matyas.fodor@gmail.com>
2022-12-16 18:08:17 +0100

    feat: day14 animation

--- src/day14/main.py
+++ src/day14/main.py
@@ -1,10 +1,40 @@
-
-
-
 from itertools import pairwise
+from PIL import Image, ImageDraw
 
 
-def draw(x_min, y_min, x_max, y_max, line_set, settlement, current_sand):
+class Animation:
+    def __init__(self, cell_size) -> None:
+        self.frames = []
+        self.cell_size = cell_size
+
+    def add_frame(self, mx):
+        height = len(mx)
+        width = len(mx[0])
+        image = Image.new("RGB", (width*self.cell_size, height*self.cell_size), "black")
+        draw = ImageDraw.Draw(image)
+        for i, line in enumerate(mx):
+            for j, e in enumerate(line):
+                if e == '#':
+                    draw.rectangle((j*self.cell_size, i*self.cell_size, (j+1) * self.cell_size, (i+1) * self.cell_size), fill=(165, 99, 41))
+                elif e == 'o':
+                    draw.rectangle((j*self.cell_size, i*self.cell_size, (j+1) * self.cell_size, (i+1) * self.cell_size), fill=(125, 125, 255))
+                elif e == '+':
+                    draw.rectangle((j*self.cell_size, i*self.cell_size, (j+1) * self.cell_size, (i+1) * self.cell_size), fill=(0, 0, 255))
+                # draw.text((j*self.cell_size, i*self.cell_size), text=str(e))
+        self.frames.append(image)
+
+    def export(self, fname):
+        frame_one = self.frames[0]
+        subsample = None
+        frames = [f for i, f in enumerate(self.frames) if subsample is None or i % subsample == 0]
+        print('start save')
+        frame_one.save(fname, format="GIF", append_images=frames,
+                    save_all=True, duration=50, loop=0)
+        print('saved')
+        
+
+
+def draw(x_min, y_min, x_max, y_max, line_set, settlement, current_sand, animation):
     mx = []
     for i in range(x_min, x_max + 1):
         mx_line = []
@@ -19,9 +49,10 @@ def draw(x_min, y_min, x_max, y_max, line_set, settlement, current_sand):
                 elem = '.'
             mx_line.append(elem)
         mx.append(mx_line)
+    mx = list(zip(*mx))
+    animation.add_frame(mx)
 
     def format(mx):
-        mx = zip(*mx)
         return '\n'.join(''.join(l) for l in mx)
     return format(mx)
 
@@ -29,6 +60,7 @@ def coord_add(a,b):
     return (a[0] + b[0], a[1] + b[1])
 
 def main(fname, second):
+    animation = Animation(10)
     lines = []
     line_set = set()
     with open(fname, 'r') as fp:
@@ -65,13 +97,17 @@ def main(fname, second):
 
     current_sand = (500, 0)
     settlement = set()
+    cntr = 0
     while True:
+        print(cntr)
+        cntr+=1
         # TODO do not break here for second
         if not second and current_sand[1] > y_max:
             break
         if second and (500, 0) in settlement:
             break
-        # print(draw(x_min, y_min, x_max, y_max, line_set, settlement, current_sand))
+        if cntr % 25 == 0:
+            draw(x_min, y_min, x_max, y_max, line_set, settlement, current_sand, animation)
         new_pos = coord_add(current_sand, (0, 1))
         if not (new_pos in settlement or new_pos in line_set):
             current_sand = new_pos
@@ -90,9 +126,10 @@ def main(fname, second):
         settlement.add(current_sand)
         current_sand = (500, 0)
 
+    animation.export('src/day14/animation.gif')
     return len(settlement)
 
 
 fname = 'src/day14/input.txt'
 
-print(main(fname, True))
+print(main(fname, False))
